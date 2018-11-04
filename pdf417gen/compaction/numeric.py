80cb0d8e72a7aba347173c4e2ab7e37cea294b7a
Ivan Habunek <ivan@habunek.com>
2018-11-04 12:19:56 +0100

    Prefer generators over lists

--- pdf417gen/compaction/numeric.py
+++ pdf417gen/compaction/numeric.py
@@ -10,14 +10,12 @@ from pdf417gen.util import to_base, chunks
 
 
 def _compact_chunk(chunk):
-    number = "".join([chr(x) for x in chunk])
+    number = "".join(chr(x) for x in chunk)
     value = int("1" + number)
     return to_base(value, 900)
 
 
 def compact_numbers(data):
     """Encodes data into code words using the Numeric compaction mode."""
-
-    compacted_chunks = [_compact_chunk(chunk) for chunk in chunks(data, size=44)]
-
+    compacted_chunks = (_compact_chunk(chunk) for chunk in chunks(data, size=44))
     return chain(*compacted_chunks)
